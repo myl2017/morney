5402e2e75a4be5dd4c62c37ddf9518e7bb669efa
孟艳利 <706695013@qq.com>
2020-02-27 22:10:34 +0800

    重复三次

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,6 +1,8 @@
 <template>
-    <div>
-        Statistics.vue
+    <div class="nav-wrapper">
+        <div class="content">
+            <p>Statistics.vue</p>
+        </div>
         <Nav/>
     </div>
 </template>
@@ -12,5 +14,15 @@
 </script>
 
 <style lang="scss" scoped>
+    .nav-wrapper {
+        border: 1px solid green;
+        display: flex;
+        flex-direction: column;
+        height: 100vh;
+    }
 
+    .content {
+        flex-grow: 1;
+        overflow: auto;
+    }
 </style>
