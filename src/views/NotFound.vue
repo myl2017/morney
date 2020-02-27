56bc478fc8beee0691f66d9c4b331b36b7448334
孟艳利 <706695013@qq.com>
2020-02-27 21:36:05 +0800

    404 page

---
+++ src/views/NotFound.vue
@@ -0,0 +1,19 @@
+<template>
+<!--    <div> current for 404 Not Found page! </div>-->
+    <div>
+        <div>当前页面不存在，请检查网址是否正确</div>
+        <div>
+            <router-link to="/">返回首页</router-link>
+        </div>
+    </div>
+</template>
+
+<script type="ts">
+  export default {
+    name: "NotFound"
+  };
+</script>
+
+<style lang="scss" scoped>
+
+</style>
