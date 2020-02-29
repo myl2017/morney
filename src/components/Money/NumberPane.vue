09fcb6cf184c42b5f3acd26f9d58a2e9d7607fb1
孟艳利 <706695013@qq.com>
2020-02-29 12:19:42 +0800

    文件名称规范书写

--- src/components/Money/NumberPane.vue
+++ src/components/Money/NumberPane.vue
@@ -1,23 +1,21 @@
 <template>
-    <div>
-        <div class="number-pane">
-            <div class="output">100</div>
-            <div class="buttons">
-                <button>1</button>
-                <button>2</button>
-                <button>3</button>
-                <button>删除</button>
-                <button>4</button>
-                <button>5</button>
-                <button>6</button>
-                <button>清空</button>
-                <button>7</button>
-                <button>8</button>
-                <button>9</button>
-                <button class="ok">OK</button>
-                <button class="zero">0</button>
-                <button>.</button>
-            </div>
+    <div class="number-pane">
+        <div class="output">100</div>
+        <div class="buttons">
+            <button>1</button>
+            <button>2</button>
+            <button>3</button>
+            <button>删除</button>
+            <button>4</button>
+            <button>5</button>
+            <button>6</button>
+            <button>清空</button>
+            <button>7</button>
+            <button>8</button>
+            <button>9</button>
+            <button class="ok">OK</button>
+            <button class="zero">0</button>
+            <button>.</button>
         </div>
     </div>
 </template>
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -9,9 +9,9 @@
 
 <script type="ts">
   import NumberPane from '@/components/Money/NumberPane.vue';
-  import Types from '@/components/Money/types.vue';
-  import Notes from '@/components/Money/notes.vue';
-  import Tags from '@/components/Money/tags.vue';
+  import Types from '@/components/Money/Types.vue';
+  import Notes from '@/components/Money/Notes.vue';
+  import Tags from '@/components/Money/Tags.vue';
   export default {
     name: "Money",
     components: {Tags, Notes, Types, NumberPane}
