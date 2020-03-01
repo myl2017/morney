8ddccfad26c8e5260e9d08191e29e9a4d7cb1a6d
孟艳利 <706695013@qq.com>
2020-03-01 17:18:05 +0800

    record数据放到localstroage存储

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,7 +1,7 @@
 <template>
     <Layout class-prefix="layout">
         {{record}}
-        <NumberPane @update:value="onUpdateAmount"/>
+        <NumberPane @update:value="onUpdateAmount" @submit="saveRecord"/>
         <Types :value.sync="record.type"/>
         <Notes @update:value="onUpdateNotes"/>
         <Tags :data-source.sync="tags" @update:value="onUpdateTags"/>
@@ -14,13 +14,26 @@
   import Types from "@/components/Money/Types.vue";
   import Notes from "@/components/Money/Notes.vue";
   import Tags from "@/components/Money/Tags.vue";
-  import {Component} from "vue-property-decorator";
+  import {Component, Watch} from "vue-property-decorator";
+
+  // const version = window.localStorage.getItem("version") || "0";
+  // if (version < "0.0.2") {
+  //   if (version === "0.0.1") {
+  //     //数据升级，数据迁移
+  //     recordList.forEach(record => {
+  //       record.createdAt = new Date(2020, 0, 1);
+  //     });
+  //   }
+  // }
+
+  window.localStorage.setItem("version", "0.0.1");
 
   type Record = {
     tags: string[]
     notes: string
     type: string
     amount: number
+    createdAt?: Date
   }
 
   @Component({
@@ -28,6 +41,7 @@
   })
   export default class Money extends Vue {
     tags = ["衣", "食", "住", "行"];
+    recordList: Record[] = JSON.parse(window.localStorage.getItem("recordList") || "[]");
     record: Record = {tags: [], notes: "", type: "-", amount: 0};
 
 
@@ -44,6 +58,17 @@
     onUpdateAmount(value: string) {
       console.log(value);
       this.record.amount = parseFloat(value);
+    }
+
+    saveRecord() {
+      const record2: Record = JSON.parse(JSON.stringify(this.record)); //deep clone 深拷贝
+      record2.createdAt = new Date();
+      this.recordList.push(record2);
+    }
+
+    @Watch("recordList")
+    onRecordListChange() {
+      window.localStorage.setItem("recordList", JSON.stringify(this.record));
     }
   }
 </script>
