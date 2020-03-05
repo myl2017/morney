d83931ac97005533703e8aa6d05b660b029fed0a
孟艳利 <706695013@qq.com>
2020-03-05 21:51:45 +0800

    add

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -2,38 +2,43 @@
     <Layout>
         <Tabs class-prefix="type" :data-source="recordTypeList" :value.sync="type"/>
         <Tabs class-prefix="interval" :data-source="intervalList" :value.sync="interval"/>
-
+        <ol>
+            <li v-for="(group,index) in result" :key="index">
+                <h3 class="title">{{group.title}}</h3>
+                <ol>
+                    <li v-for="item in group.items" :key="item.id" class="record">
+                        <span>{{tagString(item.tags)}}</span>
+                        <span class="notes">{{item.notes}}</span>
+                        <span>￥{{item.amount}}</span>
+                    </li>
+                </ol>
+            </li>
+        </ol>
     </Layout>
 </template>
 
 <style scoped lang="scss">
-    /*::v-deep .type-tabs-item, ::v-deep .interval-tabs-item { !*deep 语法*!*/
-    /*    background: white;*/
+    %item {
+        padding: 8px 16px;
+        line-height: 24px;
+        display: flex;
+        justify-content: space-between;
+        align-content: center;
+    }
 
-    /*    &.selected {*/
-    /*        background: #c4c4c4;*/
+    .title {
+        @extend %item;
+    }
 
-    /*        &::after {*/
-    /*            display: none;*/
-    /*        }*/
-    /*    }*/
-    /*}*/
+    .record {
+        background: white;
+        @extend %item;
+    }
 
-    ::v-deep { /*deep 语法*/
-        .type-tabs-item {
-            background: white;
-            &.selected {
-                background: #c4c4c4;
-
-                &::after {
-                    display: none;
-                }
-            }
-        }
-
-        .interval-tabs-item {
-            height: 48px!important;
-        }
+    .notes {
+        margin-right: auto;
+        margin-left: 16px;
+        color: #999;
     }
 </style>
 
@@ -49,6 +54,31 @@
     components: {Tabs, Types}
   })
   export default class Statistics extends Vue {
+    tagString(tags: Tag[]) {
+      return tags.length === 0 ? "无" : tags.join(",");
+    }
+
+    get recordList() {
+      return (this.$store.state as RootState).recordList;
+    }
+
+    get result() {
+      const {recordList} = this;
+      type HashTableValue = { title: string, items: RecordList[] }
+      const hashTable: { [key: string]: HashTableValue } = {};
+
+      for (let i = 0; i < recordList.length; i++) {
+        const [date, time] = recordList[i].createdAt!.split("T");
+        hashTable[date] = hashTable[date] || {title: date, items: []};
+        hashTable[date].items.push(recordList[i]);
+      }
+      return hashTable;
+    }
+
+    beforeCreate() {
+      this.$store.commit("fetchRecords");
+    }
+
     type = "-";
     interval = "day";
     intervalList = intervalList;
@@ -67,5 +97,39 @@
     .content {
         flex-grow: 1;
         overflow: auto;
+    }
+</style>
+
+<style scoped lang="scss">
+    /*::v-deep .type-tabs-item, ::v-deep .interval-tabs-item { !*deep 语法*!*/
+    /*    background: white;*/
+
+    /*    &.selected {*/
+    /*        background: #c4c4c4;*/
+
+    /*        &::after {*/
+    /*            display: none;*/
+    /*        }*/
+    /*    }*/
+    /*}*/
+
+    ::v-deep { /*deep 语法*/
+        .type-tabs-item {
+            background: white;
+
+            &.selected {
+                background: #c4c4c4;
+
+                &::after {
+                    display: none;
+                }
+            }
+        }
+
+        .interval-tabs-item {
+            height: 48px !important;
+            outline: 1px solid red;
+            font-size: 18px;
+        }
     }
 </style>
